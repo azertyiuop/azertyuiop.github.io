1339adeeabf7e9ceedcbda5d9b77b43f8c190dbe
azertyiuop <leyiseverywhere@gmail.com>
2025-12-27 22:10:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 const GITHUB_CONFIG = {
-    token: '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', // Votre token GitHub
+    token: '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', // TOKEN GIT
     gistId: null
 };
+
+// Exposer les valeurs sur window pour que common.js puisse les utiliser
+window.GITHUB_TOKEN = GITHUB_CONFIG.token;
+window.GITHUB_GIST_ID = GITHUB_CONFIG.gistId;
